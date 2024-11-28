58ade8bca6173a4b8ce61de3ab4b72782fcd0717
Roromain <romainmonmarche@gmail.com>
2024-11-28 10:22:10 +0100

    Navbar connected/not connected

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,22 +1,36 @@
-import React from 'react'
-import { NavLink } from 'react-router-dom'
+import React, { useContext } from 'react';
+import { NavLink } from 'react-router-dom';
+import { AuthContext } from '../context/AuthContext';
 
 export const Navbar = () => {
+  const { v_isConnected: isConnected, logout } = useContext(AuthContext);
+
   return (
     <nav>
       <ul>
-        <div className='navbar-container'>
-            <div className="navbar">
-                <NavLink to="/" className={({ isActive }) => (isActive ? "active-link" : "inactive-link")}>Home</NavLink>
-                <NavLink to="/cv" className={({ isActive }) => (isActive ? "active-link" : "inactive-link")}>CV</NavLink>
-                <NavLink to="/create/cv" className={({ isActive }) => (isActive ? "active-link" : "inactive-link")}>Créer un cv</NavLink>
-            </div>
-            <div>
-                <NavLink to="/register" className={({ isActive }) => (isActive ? "active-link" : "inactive-link")}>Register</NavLink>  
-                | <NavLink to="/login" className={({ isActive }) => (isActive ? "active-link" : "inactive-link")}>Login</NavLink>
-            </div>
+        <div className="navbar-container">
+          <div className="navbar">
+            <NavLink to="/" className={({ isActive }) => (isActive ? "active-link" : "inactive-link")}>Home</NavLink>
+            <NavLink to="/cv" className={({ isActive }) => (isActive ? "active-link" : "inactive-link")}>CV</NavLink>
+            {isConnected && (
+              <NavLink to="/create/cv" className={({ isActive }) => (isActive ? "active-link" : "inactive-link")}>Create CV</NavLink>
+            )}
+          </div>
+          <div>
+            {isConnected ? (
+              <div>
+                <NavLink to="/profile" className={({ isActive }) => (isActive ? "active-link" : "inactive-link")}>Profile</NavLink>
+                <button onClick={logout} className="logout-button">Logout</button>
+              </div>
+            ) : (
+              <div>
+                <NavLink to="/register" className={({ isActive }) => (isActive ? "active-link" : "inactive-link")}>Register</NavLink> | 
+                <NavLink to="/login" className={({ isActive }) => (isActive ? "active-link" : "inactive-link")}>Login</NavLink>
+              </div>
+            )}
+          </div>
         </div>
       </ul>
     </nav>
-  )
-}
+  );
+};
